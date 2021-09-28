ab955770da56357ed017047fe0777c6b21dde440
xkisst00 <xkisst00@stud.fit.vutbr.cz>
2021-09-28 11:16:24 +0200

    Added modals and click handlers
    Creates modals for namespace creation and edit.

--- rdf-storage-browsing-web-interface/src/views/Namespaces.vue
+++ rdf-storage-browsing-web-interface/src/views/Namespaces.vue
@@ -10,39 +10,22 @@
                 <h1>Namespaces </h1>
               </div>
               <div class="p-col-fixed">
-                <Button @click="alert" class="p-button-sm p-button-info size" v-tooltip.right="'Create new namespace'">
+                <Button @click="openCreateModal(position)" class="p-button-sm p-button-info size" v-tooltip.right="'Create new namespace'">
                   <i class="pi pi-plus"></i>
                   </Button>
               </div>
             </div>
       </div>  
       <div class="p-col-12" style="">
-
-          <!-- <table v-if="data" class="">
-            <th class="p-col-4">Prefix</th>  
-            <th class="p-col-4">Namespace</th>  
-            <th class="p-col-4">Options</th>
-            <tr class="" v-for="i in data.results.bindings.length" :key="i">
-              <td class="p-col-4">{{data.results.bindings[i-1].prefix.value}}</td>  
-              <td class="p-col-4">{{data.results.bindings[i-1].namespace.value}}</td>  
-              <td class="p-col-4">
-                <tr>
-                  <td><button>Del</button></td>
-                  <td><button>Set</button></td>
-                </tr>
-                </td>  
-
-            </tr>  
-          </table> -->
           <DataTable  :value="tableData.data" responsiveLayout="scroll">
               <Column  field="prefix" header="Prefix"></Column>
               <Column  field="namespace" header="Namespace"></Column>
               <Column  header="Options"> 
                 <template #body="">
-                      <Button @click="alert" class="p-button-sm margin-right p-button-warning" v-tooltip.right="'Edit'">
+                      <Button @click="openEditModal(position)" class="p-button-sm margin-right p-button-warning" v-tooltip.right="'Edit'">
                         <i class="pi pi-pencil"></i>
                         </Button>
-                      <Button @click="alert" class="p-button-sm p-button-danger" v-tooltip.right="'Remove'">
+                      <Button @click="confirmRemove" class="p-button-sm p-button-danger" v-tooltip.right="'Remove'">
                         <i class="pi pi-trash"></i>
                         </Button>
 
@@ -51,6 +34,59 @@
           </DataTable>
       </div>
     </div>
+    <Toast position="bottom-center" group="bc">
+        <template #message="slotProps">
+            <div class="p-d-flex p-flex-column">
+                <div class="p-text-center">
+                    <i class="pi pi-exclamation-triangle" style="font-size: 3rem"></i>
+                    <h4>{{slotProps.message.summary}}</h4>
+                    <p>{{slotProps.message.detail}}</p>
+                </div>
+                <div class="p-grid p-fluid">
+                    <div class="p-col-6">
+                        <Button class="p-button-success" label="Yes" @click="onConfirm" />
+                    </div>
+                    <div class="p-col-6">
+                        <Button class="p-button-secondary" label="No" @click="onReject" />
+                    </div>
+                </div>
+            </div>
+        </template>
+    </Toast>
+
+    <Dialog header="New Namespace" v-model:visible="displayCreateModal" :style="{width: '50vw'}" :position="position" :modal="true">
+      <div class="p-fluid">
+          <div class="p-field">
+              <label for="prefix">Prefix</label>
+              <InputText id="prefix" type="text" />
+          </div>
+          <div class="p-field">
+              <label for="namespace">Namespace</label>
+              <InputText id="namespace" type="text" />
+          </div>
+      </div>
+      <template #footer>
+          <Button label="Create" icon="pi pi-check" @click="closeCreateModal" autofocus />
+      </template>
+    </Dialog>
+
+    <Dialog header="Edit Namespace" v-model:visible="displayEditModal" :style="{width: '50vw'}" :position="position" :modal="true">
+      <div class="p-fluid">
+          <div class="p-field">
+              <label for="prefix">Prefix</label>
+              <InputText id="prefix" type="text" />
+          </div>
+          <div class="p-field">
+              <label for="namespace">Namespace</label>
+              <InputText id="namespace" type="text" />
+          </div>
+      </div>
+      <template #footer>
+          <Button label="Save" icon="pi pi-check" @click="closeEditModal" autofocus />
+      </template>
+    </Dialog>
+    
+
   </div>
 </template>
 
@@ -60,6 +96,9 @@ import PrimeMenu from "@/components/PrimeMenu.vue"
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import Button from 'primevue/button';
+import Toast from 'primevue/toast';
+import Dialog from 'primevue/dialog';
+import InputText from 'primevue/inputtext';
 
 export default {
   name: 'Namespaces',
@@ -67,20 +106,46 @@ export default {
     PrimeMenu,
     DataTable,
     Column,
-    Button
+    Button,
+    Toast,
+    Dialog,
+    InputText
   },
   data() {
     return {
       data: null,
-      tableData: {"data":[]}
+      tableData: {"data":[]},
+      displayCreateModal: false,
+      displayEditModal: false,
+      position:'top'
     }
   },
   async mounted() {
     this.queryAllNamespaces()
   },
   methods:{
-    alert(){
-      console.log("click")
+    confirmRemove(){
+      this.$toast.add({severity: 'warn', summary: 'Are you sure?', detail: 'Proceed to confirm', group: 'bc'});
+    },
+    onConfirm() {
+      this.$toast.removeGroup('bc');
+    },
+    onReject() {
+      this.$toast.removeGroup('bc');
+    },
+    openCreateModal(position) {
+      this.position = position;
+      this.displayCreateModal = true;
+    },
+    openEditModal(position) {
+      this.position = position;
+      this.displayEditModal = true;
+    },
+    closeCreateModal() {
+      this.displayCreateModal = false;
+    },
+    closeEditModal() {
+      this.displayEditModal = false;
     },
     async queryAllNamespaces(){
       this.data = await fetch('http://127.0.0.1:8080/rdf4j-server/repositories/1/namespaces', {
@@ -92,13 +157,6 @@ export default {
           .then(res => {
             return res.json()})
           .catch((error) => console.log(error))
-      // console.log(this.data)  
-      // console.log(this.data.head.vars[0])
-      // console.log(this.data.head.vars[1])
-
-      // console.log(this.data.results.bindings[0].namespace.value)
-      // console.log(this.data.results.bindings.length)
-
 
         for (let index = 0; index < this.data.results.bindings.length; index++) {
           this.tableData.data.push({
@@ -108,7 +166,6 @@ export default {
           })
           
         }
-        // console.log(this.tableData)
     }
   }
 }
@@ -123,7 +180,6 @@ export default {
 }
 
 .size{
-  /* width: 4.5vw; */
   margin: 25px 0 0 !important;
 }
 </style>
